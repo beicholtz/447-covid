14bcadcd4fdc511ecfed2a15c18176f0ad460cde
BananaPuncher714 <banana@aaaaahhhhhhh.com>
2021-11-05 04:02:34 -0400

    Check for database file if it exists

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,42 @@ const http = require( 'http' );
 const sqlite3 = require( 'sqlite3' ).verbose();
 const statik = require( 'node-static' );
 const qs = require( 'querystring' );
+const fs = require( 'fs' );
 
-const hostname = '0.0.0.0';
-const port = 3000;
+const HOSTNAME = '0.0.0.0';
+const PORT = 3000;
 
-/* Set up database here or something */
+const DB_FILE = "./covid.db";
+
+// If the database exists, then get when it was last updated
+// Update it to the current date if behind
+// If it doesn't exist, then download the tables and parse
+fs.access( DB_FILE, fs.F_OK, ( err ) => {
+  var accessDate = new Date( 0 );
+  if ( err ) {
+    console.log( "Database not found. Creating and updating..." );
+  } else {
+    // Get the time and update it
+    stats = fs.statSync( DB_FILE );
+    accessDate = stats.mtime;
+  }
+
+  console.log( `Date is ${ accessDate }` );
+
+  const db = new sqlite3.Database( "./covid.db", ( err ) => {
+    if ( err ) {
+      console.log( err );
+    } else {
+      console.log( "Successfully connected to the database" );
+
+      // Update the database here
+    }
+  } );
+} );
 
 const fileServer = new statik.Server( './static' );
 const server = http.createServer( ( req, res ) => {
-  var content = '';
+  let content = '';
 
   req.addListener( 'data', ( data ) => {
     body += data;
@@ -26,7 +53,11 @@ const server = http.createServer( ( req, res ) => {
     let path = urlPart[ 0 ];
     let method = req.method;
 
-    if ( method === "GET" ) {
+    if ( path === "/api/getdata" && method == "GET" ) {
+      let query = qs.parse( queries );
+      // Parse the time ranges and return the appropriate data, or return an error code if unavailable
+
+    } else if ( method === "GET" ) {
       fileServer.serve( req, res, ( e, response ) => {
         if ( e ) {
           res.statusCode = 404;
@@ -42,6 +73,6 @@ const server = http.createServer( ( req, res ) => {
   } ).resume();
 } );
 
-server.listen( port, hostname, () => {
-  console.log( `Server running at http://${hostname}:${port}/` );
+server.listen( PORT, HOSTNAME, () => {
+  console.log( `Server running at http://${HOSTNAME}:${PORT}/` );
 } );
