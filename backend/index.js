82c5d182fea4ab36175e54446ecc8b77c82536e9
BananaPuncher714 <banana@aaaaahhhhhhh.com>
2021-11-06 22:21:43 -0400

    Populate the database; Serve requests via an api call

--- backend/index.js
+++ backend/index.js
@@ -8,103 +8,86 @@ const fs = require( 'fs' );
 const HOSTNAME = '0.0.0.0';
 const PORT = 3000;
 
-const DB_FILE = "./covid.db";
-var db;
+const db = new sqlite3.Database( ':memory:' );
 
-// If the database exists, then get when it was last updated
-// Update it to the current date if behind
-// If it doesn't exist, then download the tables and parse
-fs.access( DB_FILE, fs.F_OK, ( err ) => {
-  var accessDate = new Date( 0 );
-  if ( err ) {
-    console.log( "Database not found. Creating and updating..." );
-  } else {
-    // Get the time and update it
-    stats = fs.statSync( DB_FILE );
-    accessDate = stats.mtime;
-  }
+db.serialize( () => {
+  db.run( "CREATE TABLE vaccinations ( date BIGINT, fips TEXT, name TEXT, state TEXT, complete INT, complete_pct FLOAT, complete_12 INT, complete_12_pct FLOAT, complete_18 INT, complete_18_pct FLOAT, complete_65 INT, complete_65_pct FLOAT )" );
+  db.run( "CREATE TABLE transmissions ( date BIGINT, fips TEXT, name TEXT, state_name TEXT, cases TEXT, positive FLOAT, severity TEXT )" )
+} );
 
-  console.log( `Date is ${ accessDate }` );
+console.log( "Downloading transmission data..." );
+https.get( "https://data.cdc.gov/api/views/nra9-vzzn/rows.json", ( res ) => {
+  let body = "";
 
-  db = new sqlite3.Database( "./covid.db" );
-
-  // Create the tables if it doesnt exist
-  db.serialize( () => {
-    db.run( "CREATE TABLE IF NOT EXISTS vaccinations ( date BIGINT, fips TEXT, name BIGINT, state TEXT, complete INT, complete_pct FLOAT, complete_12 INT, complete_12_pct FLOAT, complete_18 INT, complete_18_pct FLOAT, complete_65 INT, complete_65_pct FLOAT )" );
-    db.run( "CREATE TABLE IF NOT EXISTS transmissions ( date BIGINT, fips TEXT, name TEXT, state_name TEXT, cases TEXT, positive FLOAT, severity TEXT )" )
+  res.on( "data", ( chunk ) => {
+    body += chunk;
   } );
 
-  // Load the data if required
-  if ( accessDate.getTime() ) {
-    console.log( "Database is already populated" )
-  } else {
-    console.log( "Downloading transmission data..." );
-    https.get( "https://data.cdc.gov/api/views/nra9-vzzn/rows.json", ( res ) => {
-      let body = "";
-
-      res.on( "data", ( chunk ) => {
-        body += chunk;
-      } );
-
-      res.on( "end", () => {
-        console.log( "Parsing transmission data..." );
-        let data = JSON.parse( body ).data;
-        // Load it into the database
-        console.log( `Inserting transmission data(${ data.length } entries)...` );
-        db.serialize( () => {
-          let statement = db.prepare( "INSERT INTO transmissions ( date, fips, name, state_name, cases, positive, severity ) VALUES ( ?, ?, ?, ?, ?, ?, ? )" );
-          for ( let i = 0; i < data.length; i++ ) {
-            statement.run( [ new Date( data[ 3 ] ).getTime(), data[ 2 ], data[ 1 ], data[ 0 ], data[ 4 ], data[ 5 ], data[ 6 ] ] );
-            if ( i % 100000 == 0 ) {
-              console.log( `Executing transmission statement ${ i }/${ data.length }` );
-            }
-          }
-          statement.finalize();
-        } );
-
-        console.log( "Done parsing transmission data" );
-      } )
-    } ).on( "error", ( e ) => {
-      console.error( e );
+  res.on( "end", () => {
+    console.log( "Parsing transmission data..." );
+    let data = JSON.parse( body ).data;
+    // Load it into the database
+    console.log( `Inserting transmission data(${ data.length } entries)...` );
+    db.serialize( () => {
+      let statement = db.prepare( "INSERT INTO transmissions VALUES ( ?, ?, ?, ?, ?, ?, ? )" );
+      for ( let i = 0; i < data.length; i++ ) {
+        let row = data[ i ];
+        let date = new Date( row[ 11 ] );
+        date.setHours( 0 );
+        date.setMinutes( 0 );
+        date.setSeconds( 0 );
+        date.setMilliseconds( 0 );
+        statement.run( [ date.getTime(), row[ 10 ], row[ 9 ], row[ 8 ], row[ 12 ], row[ 13 ], row[ 14 ] ] );
+      }
+      statement.finalize();
     } );
 
-    console.log( "Downloading vaccination data..." );
-    https.get( "https://data.cdc.gov/api/views/8xkx-amqh/rows.json", ( res ) => {
-      let vaccinationBody = "";
-
-      res.on( "data", ( chunk ) => {
-        vaccinationBody += chunk;
-      } );
-
-      res.on( "end", () => {
-        console.log( "Parsing vaccination data..." );
-        let data = JSON.parse( vaccinationBody ).data;
-
-        console.log( `Inserting vaccination data(${ data.length } entries)...` );
-        db.serialize( () => {
-          let statement = db.prepare( "INSERT INTO vaccinations ( date, fips, name, state, complete, complete_pct, complete_12, complete_12_pct, complete_18, complete_18_pct, complete_65, complete_65_pct ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )" );
-          for ( let i = 0; i < data.length; i++ ) {
-            let slice = data[ i ].slice( 8, 20 );
-            slice[ 0 ] = new Date( slice[ 0 ] ).getTime();
-            statement.run( slice );
-            if ( i % 100000 == 0 ) {
-              console.log( `Executing vaccination statement ${ i }/${ data.length }` );
-            }
-          }
-          statement.finalize();
-        } );
-
-        console.log( "Done parsing vaccination data" )
-      } );
-    } ).on( "error", ( e ) => {
-      console.error( e );
-    } );
-
-    // Set up a runnable to update the data
-    // TODO
-
-  }
+    console.log( "Done parsing transmission data" );
+  } )
+} ).on( "error", ( e ) => {
+  console.error( e );
 } );
+
+console.log( "Downloading vaccination data..." );
+https.get( "https://data.cdc.gov/api/views/8xkx-amqh/rows.json", ( res ) => {
+  let vaccinationBody = "";
+
+  res.on( "data", ( chunk ) => {
+    vaccinationBody += chunk;
+  } );
+
+  res.on( "end", () => {
+    console.log( "Parsing vaccination data..." );
+    let data = JSON.parse( vaccinationBody ).data;
+
+    console.log( `Inserting vaccination data(${ data.length } entries)...` );
+    db.serialize( () => {
+      let statement = db.prepare( "INSERT INTO vaccinations VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )" );
+      for ( let i = 0; i < data.length; i++ ) {
+        let slice = data[ i ].slice( 8, 20 );
+        let date = new Date( slice[ 0 ] );
+        date.setHours( 0 );
+        date.setMinutes( 0 );
+        date.setSeconds( 0 );
+        date.setMilliseconds( 0 );
+        slice[ 0 ] = date.getTime();
+        statement.run( slice );
+      }
+      statement.finalize();
+    } );
+
+    console.log( "Done parsing vaccination data" )
+  } );
+} ).on( "error", ( e ) => {
+  console.error( e );
+} );
+
+// TODO Set up a runnable to update the database
+
+
+function isPositiveInteger( val ) {
+  return val && val.match( /^([0-9]+)$/ );
+}
 
 const fileServer = new statik.Server( './static' );
 const server = http.createServer( ( req, res ) => {
@@ -127,8 +110,29 @@ const server = http.createServer( ( req, res ) => {
     if ( path === "/api/getdata" && method == "GET" ) {
       let query = qs.parse( queries );
       // Parse the time ranges and return the appropriate data, or return an error code if unavailable
-      // TODO
-
+      if ( isPositiveInteger( query.start ) && isPositiveInteger( query.end ) ) {
+        db.all( `SELECT DISTINCT transmissions.date, transmissions.fips, transmissions.name, vaccinations.state, transmissions.state_name, transmissions.cases, transmissions.positive, transmissions.severity, vaccinations.complete, vaccinations.complete_pct, vaccinations.complete_12, vaccinations.complete_12_pct, vaccinations.complete_18, vaccinations.complete_18_pct, vaccinations.complete_65, vaccinations.complete_65_pct FROM transmissions OUTER LEFT JOIN vaccinations ON vaccinations.date = transmissions.date AND vaccinations.fips = transmissions.fips WHERE transmissions.date >= ( ? ) AND transmissions.date <= ( ? ) ORDER BY transmissions.fips, transmissions.date`, [ query.start, query.end ], ( error, rows ) => {
+          if ( error ) {
+            console.log( error );
+            res.statusCode = 500;
+            res.setHeader( 'Content-Type', 'application/json' );
+            res.end( '{"status":500,"message":"Error querying database"}' );
+          } else {
+            res.statusCode = 200;
+            res.setHeader( 'Content-Type', 'application/json' );
+            let result = { "data": [], "headers": [ "date", "fips_code", "county_name", "state", "state_name", "cases", "positive_pct", "severity", "complete", "complete_pct", "complete_12", "complete_12_pct", "complete_18", "complete_18_pct", "complete_65", "complete_65_pct" ] };
+            for ( let i = 0; i < rows.length; i++ ) {
+              let row = rows[ i ];
+              result.data.push( [ row.date, row.fips, row.name, row.state, row.state_name, row.cases, row.positive, row.severity, row.complete, row.complete_pct, row.complete_12, row.complete_12_pct, row.complete_18, row.complete_18_pct, row.complete_65, row.complete_65_pct ] );
+            }
+            res.end( JSON.stringify( result ) );
+          }
+        } );
+      } else {
+          res.statusCode = 400;
+          res.setHeader( 'Content-Type', 'application/json' );
+          res.end( '{"status":400,"message":"Invalid params"}' );
+      }
     } else if ( method === "GET" ) {
       fileServer.serve( req, res, ( e, response ) => {
         if ( e ) {
@@ -147,9 +151,4 @@ const server = http.createServer( ( req, res ) => {
 
 server.listen( PORT, HOSTNAME, () => {
   console.log( `Server running at http://${HOSTNAME}:${PORT}/` );
-} );
-
-process.on( "SIGINT", () => {
-  server.close();
-  db.close();
 } );
