2cc8b4d31c47187cf652bdc51b69328e09bc749d
Isaac <iluvreading2000@gmail.com>
2021-11-21 23:24:17 -0500

    made sidebar collapsible

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import React from "react";
 
 class App extends React.Component {
 
-  async openSide() {
-    document.getElementById("sidebar").style.width = "25%";
-  }
-
-  async closeSide() {
-    document.getElementById("sidebar").style.width = "0";
+  async handleViewSidebar() {
+    this.setState({sidebarOpen: !this.state.sidebarOpen});
   }
   
   constructor(props) {
@@ -22,12 +18,15 @@ class App extends React.Component {
         positive_pct : 0,
         severity : 0,
         complete : 0,
+        sidebarOpen : false,
     };
     this.updateCounty = this.updateCounty.bind(this);
-}
+    this.handleViewSidebar = this.handleViewSidebar.bind(this);
+  }
   
   async updateCounty(id){
     let req = this;
+    this.setState({sidebarOpen: true});
     await fetch('http://localhost:3072/api/getdata?start=0&end=16281360000000&fips=' + id[1], {
             method: 'GET'
         }).then(function(response){
@@ -53,12 +52,12 @@ class App extends React.Component {
           <SearchBar handler={this.updateCounty}/>
           <div className="container"> 
             <Map handler={this.updateCounty} update={this.state.selectedCounty ? true : false}/>
-            {this.state.selectedCounty ? 
+            
           
-            <SideBar countyName={this.state.selectedCounty} cases={this.state.cases} positivity={this.state.positive_pct} severity={this.state.severity} vaccinations={this.state.complete} />
+            <SideBar countyName={this.state.selectedCounty} cases={this.state.cases} positivity={this.state.positive_pct} severity={this.state.severity} vaccinations={this.state.complete} isOpen={this.state.sidebarOpen} toggleSidebar={this.handleViewSidebar}/>
           
           
-            : null} 
+            
         </div>
           
 
