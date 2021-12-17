b214e9919f993c004147870853815f82afb20cf5
BananaPuncher714 <banana@aaaaahhhhhhh.com>
2021-12-17 04:51:04 -0500

    Re-wrote backend; Parse csv files for vaccination and case data; Load data into an sql file; Load from local files; Download from website if the data doesn't exist; Updated README

--- backend/README.md
+++ backend/README.md
@@ -1,11 +1,12 @@
 ## Getting started
-To run the node server, execute `npm install` then `node index.js`. Place all static files such as ones generated from a React project into a folder named `static`. Visit [https://localhost:3000](https://localhost:3072) to view the website.
+To run the node server, execute `npm install` then `node index.js`. Place all static files such as ones generated from a React project into a folder named `static`. Visit [https://localhost:3072](https://localhost:3072) to view the website.
+
+## Updating data
+Vaccination data can be downloaded from [https://data.cdc.gov/resource/8xkx-amqh.csv](https://data.cdc.gov/resource/8xkx-amqh.csv) and placed in the same directory as the `index.js` file, renamed as `vaccination-data.csv`. Additionally, the `us-counties-20XX.csv` files can be downloaded from [here](https://github.com/nytimes/covid-19-data/tree/master/rolling-averages) and placed in the same directory as well. If the `covidData.db` file isn't detected on startup, it will check for local files, but will download from the corresponding websites if not found. To update easily, stop the server, remove all local csv files and the `covidData.db` file, and restart the server.
 
 ## API Endpoints
-`/api/getdata` - Accepts `start` and `end` field queries, as UNIX milliseconds. Will return a list of arrays with dates between(inclusive) the start and end. For example, `/api/getdata?start=1628049600000&end=1628136000000` could return 
+`/api/getdata` - Accepts `start` and `end` field queries, in IS) 8601 date format. Also accepts an optional `fips` field query. Will return a list of arrays with dates between(inclusive) the start and end. For example, `/api/getdata?start=2021-12-11&end=2021-12-11&fips=78010` could return 
 ```json
-{"data":[[1628049600000,"01001","Autauga County","AL","Alabama","279.225",25.51653,"high",24.9,13900,13900,29.2,13447,31.3,4633,51.9],...],"headers":["date","fips_code","county_name","state","state_name","cases","positive_pct","severity","complete","complete_pct","complete_12","complete_12_pct","complete_18","complete_18_pct","complete_65","complete_65_pct"]}
+{"data":[["2021-12-11","78010","St. Croix","Virgin Islands","VI",0,20519,0,19504,0,6138,0,0,6.57,12.99,0,0,0],["2021-12-11","78010","St. Croix","Virgin Islands","VI",0,20519,0,19504,0,6138,0,0,6.57,12.99,0,0,0]],"headers":["date","fips","county_name","state_name","state","complete_pct","complete_12","complete_12_pct","complete_18","complete_18_pct","complete_65","complete_65_pct","cases","cases_avg","case_avg_per_100k","deaths","deaths_avg","deaths_avg_per_100k"]}
 ```
-
-## More
-The server will automatically fetch COVID data from the [CDC Vaccinations in the US Counties](https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-County/8xkx-amqh) and [CDC Level of Transmission in the US Counties](https://data.cdc.gov/Public-Health-Surveillance/United-States-COVID-19-County-Level-of-Community-T/nra9-vzzn). It updates the databases once each day.
+Omitting the `fips` field query will include all counties in the specified date range.
--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,171 @@ const sqlite3 = require( 'sqlite3' ).verbose();
 const statik = require( 'node-static' );
 const qs = require( 'querystring' );
 const fs = require( 'fs' );
+const schedule = require( 'node-schedule' );
+const fastcsv = require( 'fast-csv' );
 
+const DB_FILE = "./covidData.db";
 const HOSTNAME = '0.0.0.0';
 const PORT = 3072;
 
+const DB_EXISTS = fs.existsSync( DB_FILE );
+
 const db = new sqlite3.Database( './covidData.db' );
 
-function isPositiveInteger( val ) {
-  return val && val.match( /^([0-9]+)$/ );
+db.serialize( () => {
+  db.run( `CREATE TABLE IF NOT EXISTS vaccinations (
+    v_date DATE,
+    v_fips TEXT,
+    state TEXT,
+    complete_pct FLOAT,
+    complete_12 INT,
+    complete_12_pct FLOAT,
+    complete_18 INT,
+    complete_18_pct FLOAT,
+    complete_65 INT,
+    complete_65_pct FLOAT,
+    PRIMARY KEY ( v_date, v_fips ) )` );
+  db.run( `CREATE TABLE IF NOT EXISTS cases (
+    c_date DATE,
+    c_fips TEXT,
+    name TEXT,
+    state_name TEXT,
+    cases INT,
+    cases_avg FLOAT,
+    case_avg_per_100k FLOAT,
+    deaths INT,
+    deaths_avg INT,
+    deaths_avg_per_100k FLOAT,
+    PRIMARY KEY ( c_date, c_fips ) )`)
+} );
+
+vaccinationHeaders = [
+  'Date',
+  'FIPS',
+  undefined, undefined,
+  'Recip_State',
+  'Series_Complete_Pop_Pct',
+  undefined,
+  'Series_Complete_12Plus',
+  'Series_Complete_12PlusPop_Pct',
+  'Series_Complete_18Plus',
+  'Series_Complete_18PlusPop_Pct',
+  'Series_Complete_65Plus',
+  'Series_Complete_65PlusPop_Pct',
+  undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined
+];
+
+function repeatWithDelim( s, c, d ) {
+  r = s;
+  for ( let i = 1; i < c; i++ ) r += d + s;
+  return r;
+}
+
+function loadVaccinationData( vaccinationDataCsvStr, callback = undefined ) {
+  let vaccinationData = [];
+  fastcsv.parseString( vaccinationDataCsvStr, { headers: vaccinationHeaders } )
+    .on( 'error', ( error ) => {
+      console.error( error );
+    } )
+    .on( 'data', ( data ) => {
+      if ( data[ "Date" ] ) {
+        let splitDate = data[ "Date" ].split( "/" );
+        data[ "Date" ] = splitDate[ 2 ] + "-" + splitDate[ 0 ] + "-" + splitDate[ 1 ];
+      }
+      vaccinationData.push( Object.values( data ) );
+    } )
+    .on( 'end', ( rowCount ) => {
+      vaccinationData.shift();
+      db.serialize( () => {
+        db.run( "BEGIN TRANSACTION" );
+        for ( let i = 0; i < vaccinationData.length; i += 500 ) {
+          db.run( `INSERT INTO vaccinations VALUES ${ repeatWithDelim( "(?,?,?,?,?,?,?,?,?,?)", Math.min( 500, vaccinationData.length - i ), "," ) } ON CONFLICT ( v_date, v_fips ) DO NOTHING`, vaccinationData.slice( i, Math.min( i + 500, vaccinationData.length ) ).flat() );
+        }
+        db.run( "COMMIT" );
+      } );
+      if ( callback ) {
+        callback();
+      }
+    } );
+}
+
+function loadCaseData( caseDataCsvStr, callback = undefined ) {
+  let caseData = [];
+  fastcsv.parseString( caseDataCsvStr, { headers: false } )
+    .on( 'error', ( error ) => {
+      console.error( error );
+    } )
+    .on( 'data', ( data ) => {
+      if ( data[ 1 ] ) {
+        data[ 1 ] = data[ 1 ].split( "-" )[ 1 ];
+      }
+      caseData.push( data );
+    } )
+    .on( 'end', ( rowCount ) => {
+      caseData.shift();
+      db.serialize( () => {
+        db.run( "BEGIN TRANSACTION" );
+        for ( let i = 0; i < caseData.length; i += 500 ) {
+          db.run( `INSERT INTO cases VALUES ${ repeatWithDelim( "(?,?,?,?,?,?,?,?,?,?)", Math.min( 500, caseData.length - i ), "," ) } ON CONFLICT ( c_date, c_fips ) DO NOTHING`, caseData.slice( i, Math.min( i + 500, caseData.length ) ).flat() );
+        }
+        db.run( "COMMIT" );
+      } );
+      if ( callback ) {
+        callback();
+      }
+    } );
+}
+
+if ( !DB_EXISTS ) {
+  console.log( "Database file not detected. Loading data..." );
+  if ( fs.existsSync( "./vaccination-data.csv" ) ) {
+    console.log( "Reading vaccination data file" );
+    loadVaccinationData( fs.readFileSync( "./vaccination-data.csv" ), () => { console.log( "Done loading vaccination data" ); } );
+  } else {
+    console.log( "Downloading vaccination data" );
+    https.get( "https://data.cdc.gov/resource/8xkx-amqh.csv", ( res ) => {
+      let vaccinationBody = "";
+
+      res.on( "data", ( chunk ) => {
+        vaccinationBody += chunk;
+      } );
+
+      res.on( "end", () => {
+        console.log( "Loading vaccination data..." );
+        loadVaccinationData( vaccinationBody, () => { console.log( "Done loading vaccination data" ); } );
+      } );
+    } ).on( "error", ( e ) => {
+      console.error( e );
+    } );
+  }
+
+  for ( let i = 2020; i < 2025; i++ ) {
+    let filename = `./us-counties-${ i }.csv`;
+    if ( fs.existsSync( filename ) ) {
+      console.log( "Reading case data file for year " + i );
+      loadCaseData( fs.readFileSync( filename ), () => { console.log( "Done loading case data for year " + i ); } );
+    } else {
+      console.log( "Downloading case data for year " + i );
+      https.get( `https://raw.githubusercontent.com/nytimes/covid-19-data/master/rolling-averages/us-counties-${ i }.csv`, ( res ) => {
+        if ( res.statusCode == 404 ) {
+          console.log( "No case data for year " + i );
+        } else {
+          let caseBody = "";
+
+          res.on( "data", ( chunk ) => {
+            caseBody += chunk;
+          } );
+
+          res.on( "end", () => {
+            console.log( `Loading case data for year ${ i }...` );
+            loadCaseData( caseBody, () => { console.log( "Done loading case data for year " + i ); } );
+          } );
+        }
+      } ).on( "error", ( e ) => {
+        console.error( e );
+      } );
+    }
+  }
 }
 
 const fileServer = new statik.Server( './static' );
@@ -35,12 +192,15 @@ const server = http.createServer( ( req, res ) => {
     if ( path === "/api/getdata" && method == "GET" ) {
       let query = qs.parse( queries );
       // Parse the time ranges and return the appropriate data, or return an error code if unavailable
-      if ( isPositiveInteger( query.start ) && isPositiveInteger( query.end ) ) {
-        //`SELECT DISTINCT transmissions.date, transmissions.fips, transmissions.name, vaccinations.state, transmissions.state_name, transmissions.cases, 
-        //transmissions.positive, transmissions.severity, vaccinations.complete, vaccinations.complete_pct, vaccinations.complete_12, 
-        //vaccinations.complete_12_pct, vaccinations.complete_18, vaccinations.complete_18_pct, vaccinations.complete_65, vaccinations.complete_65_pct 
-        //FROM transmissions OUTER LEFT JOIN vaccinations ON vaccinations.date = transmissions.date AND vaccinations.fips = transmissions.fips WHERE transmissions.date >= ( ? ) AND transmissions.date <= ( ? ) AND transmissions.fips = ( ? ) ORDER BY transmissions.fips, transmissions.date`, [ query.start, query.end, query.fips ],
-        db.all(  `SELECT * from vaccinations JOIN transmissions ON vaccinations.fips = ( ? ) AND transmissions.fips = ( ? )` , [ query.fips, query.fips ], ( error, rows ) => {
+      if ( query.start && query.end ) {
+        let statement = query.fips ? `SELECT vaccinations.*, cases.* FROM vaccinations LEFT JOIN cases ON v_date = c_date AND v_fips = c_fips WHERE ( v_date >= $start AND v_date <= $end AND v_fips = $fips ) OR ( c_date >= $start AND c_date <= $end AND c_fips = $fips )
+              UNION ALL
+              SELECT vaccinations.*, cases.* FROM cases LEFT JOIN vaccinations ON v_date = c_date AND v_fips = c_fips WHERE ( v_date >= $start AND v_date <= $end AND v_fips = $fips ) OR ( c_date >= $start AND c_date <= $end AND c_fips = $fips ) AND v_date IS NULL AND v_fips IS NULL` :
+              `SELECT vaccinations.*, cases.* FROM vaccinations LEFT JOIN cases ON v_date = c_date AND v_fips = c_fips WHERE ( v_date >= $start AND v_date <= $end ) OR ( c_date >= $start AND c_date <= $end )
+              UNION ALL
+              SELECT vaccinations.*, cases.* FROM cases LEFT JOIN vaccinations ON v_date = c_date AND v_fips = c_fips WHERE ( v_date >= $start AND v_date <= $end ) OR ( c_date >= $start AND c_date <= $end ) AND v_date IS NULL AND v_fips IS NULL`
+        // Full join
+        db.all( statement, { $start: query.start, $end: query.end, $fips: query.fips }, ( error, rows ) => {
           if ( error ) {
             console.log( error );
             res.statusCode = 500;
@@ -48,12 +208,11 @@ const server = http.createServer( ( req, res ) => {
             res.end( '{"status":500,"message":"Error querying database"}' );
           } else {
             res.statusCode = 200;
-            res.setHeader("Access-Control-Allow-Origin", '*');
             res.setHeader( 'Content-Type', 'application/json' );
-            let result = { "data": [], "headers": [ "date", "fips_code", "county_name", "state", "state_name", "cases", "positive_pct", "severity", "complete", "complete_pct", "complete_12", "complete_12_pct", "complete_18", "complete_18_pct", "complete_65", "complete_65_pct" ] };
+            let result = { "data": [], "headers": [ "date", "fips", "county_name",  "state_name", "state", "complete_pct", "complete_12", "complete_12_pct", "complete_18", "complete_18_pct", "complete_65", "complete_65_pct", "cases", "cases_avg", "case_avg_per_100k", "deaths", "deaths_avg", "deaths_avg_per_100k" ] };
             for ( let i = 0; i < rows.length; i++ ) {
               let row = rows[ i ];
-              result.data.push( [ row.date, row.fips, row.name, row.state, row.state_name, row.cases, row.positive, row.severity, row.complete, row.complete_pct, row.complete_12, row.complete_12_pct, row.complete_18, row.complete_18_pct, row.complete_65, row.complete_65_pct ] );
+              result.data.push( [ row.c_date ? row.c_date : row.v_date, row.c_fips ? row.c_fips : row.v_fips, row.name, row.state_name, row.state, row.complete_pct, row.complete_12, row.complete_12_pct, row.complete_18, row.complete_18_pct, row.complete_65, row.complete_65_pct, row.cases, row.cases_avg, row.case_avg_per_100k, row.deaths, row.deaths_avg, row.deaths_avg_per_100k ] );
             }
             res.end( JSON.stringify( result ) );
           }
@@ -81,4 +240,9 @@ const server = http.createServer( ( req, res ) => {
 
 server.listen( PORT, HOSTNAME, () => {
   console.log( `Server running at http://${HOSTNAME}:${PORT}/` );
+} );
+
+process.on( "SIGINT", () => {
+  server.close();
+  db.close();
 } );
